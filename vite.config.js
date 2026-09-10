95df6217fe7be4e949061e0f142d729ae87eeb5e
Samir Afonso Margonar Junior <samirmargonarjr@MacBook-Air-de-Samir.local>
2026-09-10 17:26:12 -0300

    feat: adicionar entrada de build para mobile

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
+
+const raiz = fileURLToPath(new URL('.', import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: `${raiz}index.html`,
+        mobile: `${raiz}index-mobile.html`,
+      },
+    },
+  },
 })
